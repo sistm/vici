d4f988f7c98504c64a5d0a620d4254b066677ac7
NERESTAN Clément <cn3@dim-ub2.local>
2019-10-14 09:32:28 +0200

    Version fonctionnel local script test

--- inst/app/tests/run_shinyTest.R
+++ inst/app/tests/run_shinyTest.R
@@ -5,5 +5,6 @@ test_that("Application works", {
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
-  expect_pass(testApp("../","shinytests/mytest.R",compareImages = FALSE))
+  expect_pass(testApp("../", compareImages = FALSE))
+  #expect_pass(testApp("../","shinytests/mytest.R",compareImages = FALSE))
 })
