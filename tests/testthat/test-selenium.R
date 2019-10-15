763da15a3626dd9e5e76ab0a8d17a4cb09cfaaff
NERESTAN Clément <cn3@dim-ub2.local>
2019-10-15 10:28:39 +0200

    nouvel essai selenium

--- tests/testthat/test-selenium.R
+++ tests/testthat/test-selenium.R
@@ -4,14 +4,23 @@ library(RSelenium)
 library(testthat)
 library(wdman)
 
-rD <- rsDriver(port = 4455L, browser = c("chrome", "firefox", "phantomjs",
-                                   "internet explorer"), version = "latest", chromever = "latest",
-        geckover = "latest", iedrver = NULL, phantomver = "2.1.1",
-        verbose = FALSE, check = TRUE)
+#rD <- rsDriver(port = 4455L, browser = c("chrome", "firefox", "phantomjs",
+#                                   "internet explorer"), version = "latest", chromever = "latest",
+#        geckover = "latest", iedrver = NULL, phantomver = "2.1.1",
+#        verbose = FALSE, check = TRUE)
+
+rD <- RSelenium::rsDriver(
+  browser = "firefox",
+  extraCapabilities = list(
+    "moz:firefoxOptions" = list(
+      args = list('--headless')
+    )
+  )
+)
 
 #pht <- phantomjs(port = 4567L)
 
-remDr <- remoteDriver(browserName = "firefox",port=4455L)
+remDr <- rD$client#remoteDriver(browserName = "firefox",port=4455L)
 remDr$open(silent = FALSE)
 appURL <- "http://127.0.0.1:8080"
 #vici::run_app()
@@ -20,11 +29,10 @@ test_that("can connect to app", {
   webElem <- remDr$findElement(using = "xpath", value = "/html/body/div[2]/h2")
   textWebElem <- webElem$getElementText()
   #appTitle <- remDr$getTitle()[[1]]
-  #cat("textWebElem => ")
-  #cat(as.character(textWebElem),"\n")
   expect_equal(as.character(textWebElem), "VICI: accurate estimation of Vaccine Induced Cellular Immunogenicity with bivariate modeling")  
 })
 
 remDr$close()
+rD$server$stop()
 #pht$stop()
-rD[["server"]]$stop()
+#rD[["server"]]$stop()
