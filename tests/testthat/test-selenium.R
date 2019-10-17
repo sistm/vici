1610db87288792d92dd3a55b575bae41ee6c1e61
NERESTAN Clément <cn3@dim-ub2.local>
2019-10-17 14:55:08 +0200

    Tests Selenium normalement fonctionnel

--- tests/testthat/test-selenium.R
+++ tests/testthat/test-selenium.R
@@ -1,36 +1,36 @@
 context("Selenium Test")
 
-#library(RSelenium)
-#library(testthat)
+library(RSelenium)
+library(testthat)
 #library(future)
 
 
 #skip_on_cran()
 
 
-#rD <- RSelenium::rsDriver(
-#  browser = "firefox",
-#  extraCapabilities = list(
-#    "moz:firefoxOptions" = list(
-#      args = list('--headless')
-#    )
-#  )
-#)
+rD <- RSelenium::rsDriver(
+  browser = "firefox",
+  extraCapabilities = list(
+    "moz:firefoxOptions" = list(
+      args = list('--headless')
+    )
+  )
+)
 
 
-#remDr <- rD$client#remoteDriver(browserName = "firefox",port=4455L)
-#remDr$open(silent = FALSE)
-#appURL <- "http://127.0.0.1:8080"
+remDr <- rD$client#remoteDriver(browserName = "firefox",port=4455L)
+remDr$open(silent = FALSE)
+appURL <- "http://127.0.0.1:8080"
 #app %<-% vici::run_app()
 
 
-#test_that("can connect to app", {
-#  skip_on_cran()
-#  remDr$navigate(appURL)
-#  webElem <- remDr$findElement(using = "xpath", value = "/html/body/div[2]/h2")
-#  textWebElem <- webElem$getElementText()
-#  expect_equal(as.character(textWebElem), "VICI: accurate estimation of Vaccine Induced Cellular Immunogenicity with bivariate modeling")  
-#})
+test_that("can connect to app", {
+  #skip_on_cran()
+  remDr$navigate(appURL)
+  webElem <- remDr$findElement(using = "xpath", value = "/html/body/div[2]/h2")
+  textWebElem <- webElem$getElementText()
+  expect_equal(as.character(textWebElem), "VICI: accurate estimation of Vaccine Induced Cellular Immunogenicity with bivariate modeling")  
+})
 
-#remDr$close()
+remDr$close()
 #rD$server$stop()
