7baa4a41d51b29c20c3124237b035813e4aa38f3
NERESTAN Clément <cn3@dim-ub2.local>
2019-10-18 15:05:32 +0200

    Add more info in test

--- tests/testthat/test-selenium.R
+++ tests/testthat/test-selenium.R
@@ -28,6 +28,13 @@ appURL <- "http://127.0.0.1:8080"
 test_that("can connect to app", {
   skip_on_cran()
   remDr$navigate(appURL)
+  url <- remDr$getCurrentUrl()
+  cat("url => ")
+  cat(url,"\n")
+  cat("page title =>")
+  cat(remDr$getTitle(appURL),"\n")
+  cat("Page Source => ")
+  cat(remDr$getPageSource(),"\n")
   webElem <- remDr$findElement(using = "xpath", value = "/html/body/div[2]/h2")
   textWebElem <- webElem$getElementText()
   expect_equal(as.character(textWebElem), "VICI: accurate estimation of Vaccine Induced Cellular Immunogenicity with bivariate modeling")  
