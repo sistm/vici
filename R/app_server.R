3ea8972dd51393fced79d6e112e24bdf901a7c41
cneresta <clement.nerestan@gmail.com>
2022-06-13 17:28:39 +0200

    Update for LK 20.11

--- R/app_server.R
+++ R/app_server.R
@@ -74,9 +74,9 @@ app_server <- function(input, output, session) {
       set <<- paste0("apikey|",key)
       
       Rlabkey::labkey.setDefaults(apiKey=set)#"apikey|73ea3ff0973f38d52f5b1bbd8980f62c")
-      Rlabkey::labkey.setDefaults(baseUrl = "https://labkey.bph.u-bordeaux.fr/")#(baseUrl="https://labkey.bph.u-bordeaux.fr:8443/")
+      Rlabkey::labkey.setDefaults(baseUrl = "https://labk.bph.u-bordeaux.fr/")#(baseUrl="https://labkey.bph.u-bordeaux.fr:8443/")
       labkey.data <- labkey.selectRows(
-        baseUrl="https://labkey.bph.u-bordeaux.fr", 
+        baseUrl="https://labk.bph.u-bordeaux.fr", 
         #folderPath="/EBOVAC/assays/EBL2001/ICS", 
         folderPath=subF,  #"/VASI/VICI/SISTM",
         schemaName=paste0("assay.General.",assay),#"assay.General.Vici_Sistm", 
