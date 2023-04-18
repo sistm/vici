57ce1eb33b4170fafc7e0f22acea952dda7694c8
HUCHON Mélanie <mh8@dim-ub2.local>
2023-04-18 13:53:59 +0200

    change url for issues link

--- R/app_ui.R
+++ R/app_ui.R
@@ -7,7 +7,7 @@ app_ui <- function() {
 
     shiny::actionLink(inputId='github_code', label="Source code",
                       icon = icon("github"),
-                      onclick ="window.open('https://github.com/borishejblum/vici', '_blank')"),
+                      onclick ="window.open('https://github.com/sistm/vici', '_blank')"),
     shiny::actionLink(inputId='github_code', label="CRAN package",
                       icon = icon("r-project"),
                       onclick ="window.open('https://CRAN.R-project.org/package=vici', '_blank')",
